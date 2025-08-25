aeb1e3394b09682eb3a9b4a613aa1def65dee7b3
Irfan <irfannaqieb@gmail.com>
2025-08-25 14:16:38 +0800

    feat: add development server settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
 	ssr: false,
 	devtools: { enabled: true },
+	devServer: {
+		host: "0.0.0.0",
+		port: 3000,
+	},
 	modules: [
 		// '@nuxtjs/tailwindcss',
 		"@nuxt/ui",
